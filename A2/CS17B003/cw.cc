2b61ea4db46cbe6ea77d0a0d66001697f2dcacf1
Anirudh S <anirudh6626@gmail.com>
2021-03-29 12:06:31 +0530

    Simulator: Added AIMD simulation

--- A2/CS17B003/cw.cc
+++ A2/CS17B003/cw.cc
@@ -1,5 +1,74 @@
 #include <bits/stdc++.h>
+#define MSS 1.0             // MSS size in KB
+#define RWS 1024.0          // RWS size in KB
+#define THRESH_MULT 0.5     // Threshold multiplier for every timeout
 using namespace std;
+
+/**
+ * AIMD simulator
+ * Arguments:
+ *      ki - initial CW size (in MSS)
+ *      km - exponential growth multiplier
+ *      kn - linear growth multiplier
+ *      kf - timeout multiplier
+ *      ps - probability of timeout
+ *      T - total number of updates
+ *      outfile - output file name
+*/
+void simulate_aimd(double ki, double km, double kn, double kf, double ps, int T, string outfile) {
+    double cw, threshold;       // Congestion window size and congestion threshold
+    cw = ki * MSS;              // Initial command window size
+    threshold = DBL_MAX;        // Set to infinity
+
+    ofstream output;
+    output.open(outfile, ios::out | ios::trunc);
+    output << cw << "\n";       // Print initial CW size
+
+    int num_upd = 0;
+    bool slow_start = true;     // Indicates whether protocol is in slow start or congestion avoidance
+
+    // Seed random number generator
+    srand((unsigned)time(NULL));
+
+    while(num_upd < T) {
+        // Send segments in the command window
+        for(int i = 0; i < ceil(cw / MSS); i++) {
+            // Generate random probability
+            double prob = (double)rand()/ RAND_MAX;
+            if(prob < ps) {
+                // Timeout; congestion detection phase
+                threshold = THRESH_MULT * cw;   // Update threshold
+                cw = max(1.0, kf * cw);         // Update CW size
+                slow_start = true;              // Restart from slow start phase
+
+                output << cw << "\n";
+                num_upd++;
+                break;
+            }
+            
+            // Packet acknowledged successfully; update window size accordingly
+            if(slow_start) {
+                // In slow start phase
+                cw = min(cw + km * MSS, RWS);
+
+                // Check if CW size has reached threshold
+                if(cw >= threshold) {
+                    slow_start = false;
+                }
+            }
+            else {
+                // In congestion avoidance phase
+                cw = min(cw + (kn * MSS * MSS / cw), RWS);
+            }
+
+            output << cw << "\n";
+            if(++num_upd >= T)
+                break;
+        }
+    }
+
+    
+}
 
 int main(int argc, char* argv[]) {
     double ki, km, kn, kf, ps;
@@ -78,13 +147,8 @@ int main(int argc, char* argv[]) {
         }
     }
     
-    cout << "ki = " << ki << "\n";
-    cout << "km = " << km << "\n";
-    cout << "kn = " << kn << "\n";
-    cout << "kf = " << kf << "\n";
-    cout << "ps = " << ps << "\n";
-    cout << "T = " << t << "\n";
-    cout << "outfile = " << out << "\n";
+    // Invoke simulator
+    simulate_aimd(ki, km, kn, kf, ps, t, out);
 
     return 0;
 }
