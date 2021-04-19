fd7b7a0dda027b7043aa471fa932f0c37ff0a92e
Anirudh S <anirudh6626@gmail.com>
2021-04-19 23:47:57 +0530

    Input file parsing done

--- A3/ospf.cc
+++ A3/ospf.cc
@@ -4,7 +4,17 @@ using namespace std;
 int id, hi, lsai, spfi;
 string infile, outfile;
 
+int num_nodes, num_edges;
+struct node_info {
+    int id;
+    int wt;
+    int min_wt;
+    int max_wt;
+};
+vector<node_info> neighbours;
+
 void parse_args(int argc, char* argv[]);
+void read_file();
 
 void hello_send() {
     while(true) {
@@ -59,6 +69,9 @@ int main(int argc, char* argv[]) {
 
     cout << id << " " << infile << " " << outfile << " ";
     cout << hi << " " << lsai << " " << spfi << "\n";
+
+    // Read info from the file
+    read_file();
 
     // Run ospf
     run();
@@ -127,6 +140,46 @@ void parse_args(int argc, char* argv[]) {
                 cout << "Incorrect input format\n";
                 exit(0);
             }
+        }
+    }
+}
+
+void read_file() {
+    ifstream file(infile);
+    string line;
+    string word;
+    stringstream ss;
+
+    // First line
+    getline(file, line);
+    ss << line;
+    ss >> word;
+    num_nodes = stoi(word);
+    ss >> word;
+    num_edges = stoi(word);
+    ss.clear();
+
+    // Edges
+    for(int i = 0; i < num_edges; i++) {
+        getline(file, line);
+        ss << line;
+        ss >> word;
+        int node1 = stoi(word);
+        ss >> word;
+        int node2 = stoi(word);
+        ss >> word;
+        int min_wt = stoi(word);
+        ss >> word;
+        int max_wt = stoi(word);
+        ss.clear();
+
+        if(node1 == id) {
+            node_info nbr = {node2, INT_MAX, min_wt, max_wt};
+            neighbours.push_back(nbr);
+        }
+        else if(node2 == id) {
+            node_info nbr = {node1, INT_MAX, min_wt, max_wt};
+            neighbours.push_back(nbr);
         }
     }
 }
